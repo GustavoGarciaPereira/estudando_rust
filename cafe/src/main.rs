614f17704db6bab4b58d65f8ef6dc3b722783110
gustavoGarciaPereira <gusgurtavo@gmail.com>
2021-03-25 20:42:28 -0300

    hello rust

---
+++ cafe/src/main.rs
@@ -0,0 +1,10 @@
+/// # meu primeiro código em rust
+/// - que linguagem é essa meu deus me apaixonei
+
+fn main() {
+    let name = "gustavo";
+    let emoji = "🦀 ⚙️ ";
+    let _colo = vec![12,23];
+    println!("hello, gustavo no rust!\n\n  ({})\n\n({})",emoji,name);
+
+}
