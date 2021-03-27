181148b93b07e594f5ddb848ab8496c4e98f5a3c
gustavoGarciaPereira <gusgurtavo@gmail.com>
2021-03-26 21:15:34 -0300

    funcao generica

--- funcao/src/main.rs
+++ funcao/src/main.rs
@@ -6,12 +6,32 @@ fn funcao_generica<T>(qualquer_coisa:T) -> T{
     qualquer_coisa
 }
 
+//funcao recenbendo funcao
+fn decorador<F>(func:F, n:i32)->i32
+where F:Fn(i32) -> i32{
+    func(n)
+}
+
+
 fn main() {
+
+    fn sub(num:i32) -> i32{
+        num-10
+    }
+
+    let a = |x| x*2;
+    println!("{}",a(23));
+
     assert_eq!(funcao_generica(2), 2);
     assert_eq!(funcao_generica(true), true);
     assert_eq!(funcao_generica("guguzinho"), "guguzinho");
 
     assert_eq!(retorna_um(1,2),3);
+
+    assert_eq!(decorador(sub,42),32);
+    println!("<decorador-> {}",decorador(sub,50));
+
+    
     println!("{}",retorna_um(2,3));
     println!("Hello, world!");
 }
